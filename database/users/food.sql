6eedf482e0bba22fe52f535bb5e7d16739eb8ef8
armando-dev <ah25632@gmail.com>
2021-11-27 17:24:14 -0600

    "add fields to insert in DB"

---
+++ database/users/food.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (1,'Tacos',30.00,'Tacos de muerta lenta, la orden contiene 10 tacos','2021-06-05 02:10:39','2021-06-05 02:10:39');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (2,'Tlayuda',60.00,'De todos los sabores que haya en el mercado','2021-09-01 07:55:42','2021-09-01 07:55:42');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (3,'Totopos',40.00,'Con mucho queso y tortilla recien salida-','2021-04-26 03:10:10','2021-04-26 03:10:10');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (4,'Mole',70.00,'El plato cuesta 70, es de Tlaxcala.','2021-06-12 22:12:08','2021-06-12 22:12:08');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (5,'Escamoles',40.00,'Son los mejores de la zona','2021-10-20 03:36:42','2021-10-20 03:36:42');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (6,'Sopa de lima',90.00,'La mejor sopa de la zona con muchas verduras','2021-08-22 18:12:52','2021-08-22 18:12:52');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (7,'Tamales',10.00,'Son verde, mole y rajas.','2021-04-20 04:52:16','2021-04-20 04:52:16');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (8,'Enchiladas',70.00,'COn mucho queso y salsa verde.','2021-01-18 14:49:42','2021-01-18 14:49:42');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (9,'Pozole',120.00,'Pozole tipico como lo hacian nuestros ante-pasados.','2021-09-05 10:09:25','2021-09-05 10:09:25');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (10,'Quesadillas',50.00,'Solo de carne o campechana.','2021-09-25 09:42:27','2021-09-25 09:42:27');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (11,'Aguachiles',50.00,'Son chiles con agua','2021-09-18 10:40:09','2021-09-18 10:40:09');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (12,'Chiles en Nogada',150.00,'Viene relleno con mucha carne y los sabores tipicos.','2021-10-09 15:37:45','2021-10-09 15:37:45');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (13,'Carnitas',140.00,'1KG por $140 con 1kg de tortillas y refresco','2021-05-09 07:49:13','2021-05-09 07:49:13');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (14,'Barbacoa',250.00,'Es de firulais','2021-11-25 10:24:41','2021-11-25 10:24:41');
+INSERT INTO `food`  (`id`,`name`,`price`,`description`,`created_at`,`updated_at`) VALUES (15,'Tacos de canasta',10.00,'Tacos de tinga y chicharron','2021-11-27 15:20:59','2021-11-27 15:20:59');
